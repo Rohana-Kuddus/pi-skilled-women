739b1318f7c193b3c191be7972acb26f7e491f1e
RizqiAmaliyyah <rizqia439@gmail.com>
2023-12-06 18:26:57 +0700

    perbaikan lagi

--- Desain-Grafis/roadmapdesain.js
+++ Desain-Grafis/roadmapdesain.js
@@ -1,71 +1,56 @@
-// Fungsi untuk membuka offcanvas
 function openOffcanvas() {
   var offcanvas = document.getElementById('offcanvasRight');
   var offcanvasBS = new bootstrap.Offcanvas(offcanvas);
   offcanvasBS.show();
 }
 
-// Menambahkan event listener pada elemen SVG
 document.getElementById('sidebarTrigger').addEventListener('click', openOffcanvas);
 
 
 // Api
-// Fetch Pokémon data from the provided API
-fetch('https://pokeapi.deno.dev/pokemon?limit=10')
-  .then(response => response.json())
-  .then(data => {
-    // Access the array of Pokémon objects
-    const pokemonArray = data;
+const getJob = async (id) => {
+  const api = `https://65687b5f9927836bd974e7bb.mockapi.io/api/v1/jobs/${id}`;
 
-    // Get the row container
-    const rowContainer = document.getElementById('daftar-kelas');
+  const raw = await fetch(api);
+  const data = await raw.json();
+  return data;
+};
 
-    // Loop through each Pokémon in the array
-    pokemonArray.forEach(pokemon => {
-      // Create Bootstrap card for each Pokémon
-      const pokemonElement = document.createElement('div');
-      pokemonElement.className = 'card mb-3'; // Adjust the column size as needed
-      pokemonElement.style.maxWidth = "540px";
-      pokemonElement.innerHTML = `
-    
+const getDetail = async (id) => {
+  const api = `https://65695a6ade53105b0dd6ef91.mockapi.io/jobs/details/${id}`;
+  
+  const raw = await fetch(api);
+  const data = await raw.json();
+  return data;
+};
 
-      <div class="row g-0">
-        <div class="col-md-4">
-          <!-- tambahkan gambar -->
-          <img src="${pokemon.imageUrl}" class="img-fluid rounded-start" alt="${pokemon.name}">
-        </div>
+const getkelasDesain = async (id) => {
+  const api =`https://65702b3609586eff6640d32c.mockapi.io/roadmaprizqi/${id}`;
 
-        <div class="col-md-8">
-          <!-- deskripsi -->
-          <div class="card-body">
-            <h5 class="heading3-green">${pokemon.name}</h5>
-            <div class="tag">
-              <p class="btn-secondary">${pokemon.types}</p>
-            </div>
-            <p class="text-small-dark">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div class="row card-buttons">
-              <div class="col vote d-flex gap-2">
-                  <div class="upvote">
-                      <i class="ri-thumb-up-fill"> 58</i>
-                  </div>
-                  <div class="downvote">
-                      <i class="ri-thumb-down-line"> </i>
-                  </div>
-              </div>
-              <div class="col btn-kelas">
-                  <button class="btn-primary-green">Lihat Kelas</button>
-              </div>
-            </div>
-          </div>
-        </div>
+  const raw = await fetch(api);
+  const data = await raw.json();
+  return data;
+};
 
-      </div>
-  <!-- card end -->
+const showData = async () => {
+  // get data by id
+  // const params = new URLSearchParams(window.location.search);
+  // const id = params.get('id');
+  const id = '2';
+  const job = await getJob(id);
+  const detail = await getDetail(id);
+  const desain = await getkelasDesain(id);
 
-            `;
-        console.log(rowContainer.firstElementChild);
-      // Append the Pokémon card to the row container
-      rowContainer.appendChild(pokemonElement);
-    });
-  })
-  .catch(error => console.error('Error fetching Pokémon data:', error));
+  console.log(detail.desain.hidroponik);
+  // const title = document.getElementById('job-title');
+  // title.innerHTML = job.title;
+
+  // const img = document.getElementById('hero-img');
+  // img.src = job.image;
+  // img.alt = job.title.slice(6, job.title.length - 4);
+
+  // const penjelasan = document.getElementById('roadmap');
+  // penjelasan.innerHTML = job.description;
+};
+
+showData();
